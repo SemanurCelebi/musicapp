e7edecab8dcc08c52bd4f2cbac381ed70d66360f
Semanur <semanure38@gmail.com>
2024-09-16 13:45:33 +0200

    deploy try

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/wordsapp/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 });
