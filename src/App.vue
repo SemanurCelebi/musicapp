55acaa1fbe8518013f25b5e8f6311545f035d01b
Semanur <semanure38@gmail.com>
2024-10-14 14:23:25 +0200

    changed repo name

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="flex items-center justify-center min-h-screen bg-blue-100">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did itttt!" />
     </div>
   </header>
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/wordsapp/',
+  base: '/musicapp/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
